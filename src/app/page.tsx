09b2ec72eb3fdfe7f08b59b1777670044f16dbe7
shikhar360 <sssonishikhar360@gmail.com>
2023-10-08 15:20:07 +0530

    added rainbowkit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import Chat from './(xmtp)/chat/page'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import Homepage from './_components/Homepage'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-white text-black p-24">
-     hello world
-     <Chat/>
+     <Homepage/>
+     {/* <ConnectButton/> */}
+     {/* <Chat/> */}
     </main>
   )
 }
